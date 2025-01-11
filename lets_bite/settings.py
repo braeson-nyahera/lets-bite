529e718e27b15d189045a73a4c9c98f1fb1c8d7a
braeson-nyahera <braebulimo@gmail.com>
2025-01-11 21:48:28 +0300

    azure database

--- lets_bite/settings.py
+++ lets_bite/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#sk9su^2v^o8r070_vig*v!jc0-#cuhz=+ri6ep=8g4b@%2^&@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
