ed770b6374c4d4782555bc0467421fa863f51b99
braeson-nyahera <braebulimo@gmail.com>
2025-01-20 12:33:07 +0300

    change db

--- user/models.py
+++ user/models.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class CustomUser(AbstractUser):
-    email = models.EmailField(max_length=50, blank=True, null=True)
-    phone_number = models.IntegerField(blank=True, null=True)
+    email = models.EmailField(unique=True,max_length=50, blank=True, null=True)
+    phone_number = models.IntegerField(unique=True,blank=True, null=True)
 
     def __str__(self):
         return self.email
